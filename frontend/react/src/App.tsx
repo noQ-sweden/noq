b7961dacec730c6a1271fd97e66f84a838cd8324
Martin Jonsing <martin.jonsing@sandvik.com>
2023-04-15 21:36:43 +0200

    Small change to test CD pipeline

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -18,9 +18,6 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
     </div>
   )
