4c05e140987d3b0d9650f54c996282cc1cf6714f
Linus Åkesson <linusakesson3@gmail.com>
2023-04-18 14:14:00 +0200

    Adding spring boot

---
+++ backend/src/main/java/help/noqsweden/noq/NoQApplication.java
@@ -0,0 +1,13 @@
+package help.noqsweden.noq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoQApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/help/noqsweden/noq/NoQApplicationTests.java
@@ -0,0 +1,13 @@
+package help.noqsweden.noq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoQApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
