b24eaacd29edf67a6059bae2c4c09817a1c25043
Sebastian Kärner <sebastiankarner@live.se>
2023-04-24 19:52:49 +0200

    Created a simple Hello World Controller/Service // Sebastian Kärner

---
+++ backend/src/main/java/help/noqsweden/noq/HelloWorld/HelloWorldController.java
@@ -0,0 +1,22 @@
+package help.noqsweden.noq.HelloWorld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloworld")
+public class HelloWorldController {
+
+    private final HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+
+    @GetMapping()
+    public String helloWorld() {
+        return helloWorldService.helloWorld();
+    }
+}
---
+++ backend/src/main/java/help/noqsweden/noq/HelloWorld/HelloWorldService.java
@@ -0,0 +1,10 @@
+package help.noqsweden.noq.HelloWorld;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
