9ea4d2091724c5096bf125e5edf55196649b088d
Eyuel <eyueltb@gmail.com>
2023-04-28 23:13:21 +0200

    new-backend

---
+++ noq-backend/src/test/java/com/noq/backend/NoqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noq.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoqBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
