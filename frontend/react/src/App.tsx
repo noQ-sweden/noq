5be30af12f16cea50f35c93ee6f3027f19ef8961
Sebastian Kärner <sebastiankarner@live.se>
2023-05-06 18:17:28 +0200

    Hello from backend!

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -3,7 +3,12 @@ import noqLogo from "/noQ.png";
 import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState("hello world");
+  const [message, setMessage] = useState("");
+
+  axios.get("https://ca-noq-backend--u32igcx.thankfulglacier-35d24b26.swedencentral.azurecontainerapps.io/api/reservation/hello ")
+      .then((res:string) => setMessage(res))
+      .catch((err:Error) => err.message);
+
  
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-gray-800 text-white">
