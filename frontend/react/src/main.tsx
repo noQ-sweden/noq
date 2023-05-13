576e211eeb828109c2ac515e3bdb090c0cb824bb
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-05-13 12:54:07 +0200

    Learning+Tailwind

--- frontend/react/src/main.tsx
+++ frontend/react/src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import MyReservations from './pages/myReservations'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+  <MyReservations></MyReservations>
   </React.StrictMode>,
 )
---
+++ frontend/react/src/pages/myReservations.tsx
@@ -0,0 +1,19 @@
+function MyReservations () {
+    return (
+        <>
+
+       <nav className="sm:box-border border container-3 flex justify-center bg-blue-200 h-200 p-4 m-2 text-2xl">Start</nav>
+   
+
+        <header><h1 className="container-3 border-2 border-black flex justify-center text-lg">Mina bokningar</h1></header>
+        <section>
+            <div>Bekräftade</div>
+            <div>Behandlas</div>
+        </section>
+        <main></main>
+        
+        </>
+    )
+}
+
+export default MyReservations
