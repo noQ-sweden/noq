58fb4c6aa41742f4e17ef1c8f4d2df08bc92286b
Eyuel <eyueltb@gmail.com>
2023-05-15 21:52:32 +0200

    vip reservation

--- noq-backend/src/main/java/com/noq/backend/dto/HostDTO.java
+++ noq-backend/src/main/java/com/noq/backend/dto/HostDTO.java
@@ -1,40 +1,9 @@
-package com.noq.backend.dto;
+package com.noq.backend.DTO;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.noq.backend.models.Address;
-import com.noq.backend.models.Bed;
-
-import java.math.BigDecimal;
-import java.util.HashSet;
 import java.util.UUID;
 
 
-public record HostDTO(String name, Address address, HashSet<Bed> bedIds) {
-
-    @JsonCreator
-    public HostDTO(
-            @JsonProperty("name") String name,
-            @JsonProperty("address") Address address,
-            @JsonProperty("bedIds") HashSet<Bed> bedIds) {
-        this.name = name;
-        this.address = address;
-        this.bedIds = bedIds;
-    }
-
-    @Override
-    public String name() {
-        return name;
-    }
-
-    @Override
-    public Address address() {
-        return address;
-    }
-
-    @Override
-    public HashSet<Bed> bedIds() {
-        return bedIds;
-    }
+public record HostDTO(UUID hostId, String name, Address address, String image, Long beds) {
 
 }
