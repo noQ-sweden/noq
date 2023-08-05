e8f0c1aae69ca900277178da34189bfe35acb626
frykhamn <gabriella.frykhamn@sensera.se>
2023-08-05 12:32:17 +0200

    beds

--- frontend/react/src/pages/Host/views/bedsView/BedsView.tsx
+++ frontend/react/src/pages/Host/views/bedsView/BedsView.tsx
@@ -1,11 +1,5 @@
-import {useEffect, useState} from "react";
-import {getAllHostRequests} from "../../../../api/GetAllHostRequests";
-import {IoMdSettings} from "react-icons/io";
-import {FaRegCalendarAlt} from "react-icons/fa";
-import {List, ListItem, ListItemPrefix} from "@material-tailwind/react";
+
 import * as React from "react";
-import {IReservation} from "../../../../interfaces/IReservation";
-import {approveReservations} from "../../../../api/ApproveReservations";
 import HandleBedsComponent from "../../components/HandleBedsComponent";
 
 
