8980785fe678b006c8f21737ca07d1dbe849017f
Gabriella Frykhamn <131661892+frykhamn@users.noreply.github.com>
2023-08-11 06:58:25 +0000

    new interface for gustepage

--- frontend/react/src/pages/guest/GuestPage.tsx
+++ frontend/react/src/pages/guest/GuestPage.tsx
@@ -2,10 +2,11 @@ import {SideBarComponenet} from "./components/SideBarComponent";
 import {Outlet} from "react-router";
 import {getGuest} from "../../api/GuestPageApi";
 import {useEffect, useState} from "react";
-import {IUser} from "../../interfaces/IUser";
+import {IGuestPageModel} from "../guest/IGuestPageModel"
+
 
 export default function GuestPage() {
-    const [user, setUser] = useState<IUser>({id: "", name: ""})
+    const [user, setUser] = useState<IGuestPageModel>({id: "", name: ""})
     const userId = "1"
 
     const fetchView = async () => {
---
+++ frontend/react/src/pages/guest/IGuestPageModel.ts
@@ -0,0 +1,4 @@
+export interface IGuestPageModel{
+    id: string;
+    name: string;
+}
