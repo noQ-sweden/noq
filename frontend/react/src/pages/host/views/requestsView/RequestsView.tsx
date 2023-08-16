21aa6b5ae29301a36a5d4f141e6f05b85998dd3f
frykhamn <gabriella.frykhamn@sensera.se>
2023-08-16 14:10:53 +0200

    hadnled undefined

--- frontend/react/src/pages/host/views/requestsView/RequestsView.tsx
+++ frontend/react/src/pages/host/views/requestsView/RequestsView.tsx
@@ -14,7 +14,6 @@ export default function RequestsView() {
         try {
             const response = await getAllHostRequests(hostId);
             setRequests(response?.data);
-            console.log(response.data)
         } catch (error) {
             console.error(error);
         }
