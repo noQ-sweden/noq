8444789456ac60512f7bbcc1ab4f36de86fbe4ac
SebastianKärner88 <sebastiankarner@live.se>
2023-09-05 15:16:26 +0200

    ADD - added some config files for CosmosDB.

--- noq-backend/src/main/java/com/noq/backend/configs/AzureCredentials.java
+++ noq-backend/src/main/java/com/noq/backend/configs/AzureCredentials.java
@@ -2,9 +2,7 @@ package com.noq.backend.configs;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
