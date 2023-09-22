72001e21fba797464d1aba302a50d182732a5d1c
@jimmy.brissman <jimmy.brissman@protonmail.com>
2023-09-22 10:18:47 +0200

    Cosmos Async Client
    Added the necessary dependency in the pom.xml-file for building an asynchronous cosmos client with managed identity. The bean for cosmos client builder in the CosmosSpringConfiguration has been rebuilt to (hopefully) provide a working client using the environment variables for AZURE_CLIENT_ID and COSMOS_DB_ACCOUNT_ENDPOINT in the backend-container.
    
    Added a fallback configuration for local development.

--- noq-backend/src/main/java/com/noq/backend/configs/AzureCredentials.java
+++ noq-backend/src/main/java/com/noq/backend/configs/AzureCredentials.java
@@ -2,7 +2,6 @@ package com.noq.backend.configs;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
