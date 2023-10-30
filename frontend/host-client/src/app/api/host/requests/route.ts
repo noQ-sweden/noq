5ae4dc0bf50c7561817d3dc3c97bbc280d8769f7
Kristoffer <kristoffer@email.com>
2023-10-30 17:25:50 +0100

    improved error handling

--- frontend/host-client/src/app/api/host/requests/route.ts
+++ frontend/host-client/src/app/api/host/requests/route.ts
@@ -2,12 +2,35 @@ const port = process.env.BACKEND_URL_ENDPOINT
 const requestMapping = "api/host/requests"
 
 export async function GET(request: Request) {
-  const authorization = request.headers.get("authorization");
-  if (!authorization) return Response.json({message: "No authorization header provided"});
+  try {
+    const authorization = request.headers.get("authorization");
+    if (!authorization) {
+      return new Response(JSON.stringify({ message: "No authorization header provided" }), {
+        status: 401,
+        headers: { "Content-Type": "application/json" }
+      });
+    }
 
-  const res = await fetch(`${port}/${requestMapping}`, {
-    method: "GET",
-    headers: {Authorization: authorization},
-  }).then(res => res.json());
-  return Response.json(res)
+    const res = await fetch(`${port}/${requestMapping}`, {
+      method: "GET",
+      headers: { Authorization: authorization },
+    });
+
+    const responseBody = res.ok ? await res.json() : { error: `Request failed with status ${res.status}`, message: res.statusText };
+    const status = res.ok ? 200 : res.status;
+
+    return new Response(JSON.stringify(responseBody), {
+      status,
+      headers: { "Content-Type": "application/json" }
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return new Response(JSON.stringify({
+      error: "Internal Server Error",
+      message: "An unexpected error occurred"
+    }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" }
+    });
+  }
 }
