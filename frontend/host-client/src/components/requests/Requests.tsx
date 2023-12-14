197f639bed742980e009d15823d46283bb37f5bb
frykhamn <gabriella.frykhamn@sensera.se>
2023-12-14 21:58:22 +0100

    merged main

--- frontend/host-client/src/components/requests/Requests.tsx
+++ frontend/host-client/src/components/requests/Requests.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, {useEffect, useState} from 'react';
-import {RequestsPageDTO} from "@/components/requests/RequestsPageDTO";
-import {fetchPage} from "@/components/requests/RequestsAPI";
+import {BookingReqBody, RequestsPageDTO} from "@/components/requests/RequestsPageDTO";
+import {fetchApproveRequest, fetchDenyRequest, fetchPage} from "@/components/requests/RequestsAPI";
 import Button1 from "@/libs/Button1";
 import Button1Error from "@/libs/ButtonError";
 import {useQuery} from "@tanstack/react-query";
@@ -86,26 +86,6 @@ const Requests = (props: RequestsProps) => {
                 )
               })}
             </section>
-
-            <section className={"flex flex-col gap-1"}>
-              <h1 className={"text-2xl"}>Förfrågningar</h1>
-              {requestsPageDTO && requestsPageDTO.pendingBookings.map((reservation: any) => {
-                return (
-                    <div key={reservation.id} className={"flex flex-col border-2 rounded border-zinc-500 p-1 gap-1"}>
-                      <p>Namn: {reservation.name}</p>
-                      <p>Unokod: {reservation.unoCode}</p>
-                      <div className={"flex flex-col gap-1 sm:flex-row"}>
-                        <Button1 title={"Godkänn"} isLoading={false}
-                                 onClick={() => onBtnApprove(reservation.id, reservation.userId)}/>
-                        <Button1Error title={"Neka"} isLoading={false}
-                                      onClick={() => onBtnDenied(reservation.id, reservation.userId)}/>
-                      </div>
-                    </div>
-                )
-              })}
-            </section>
-          </>}
-
         </main>
       </div>
   );
